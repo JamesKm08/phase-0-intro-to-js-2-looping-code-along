ce44a5dc7d77dd431eb8962e01f32ae2eaf27269
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-07-16 17:00:28 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    for(let i=name; i < name.length; i++){
+        const name = name[i];
+        
+        return `Thank you, ${name}, for the wonderful ${event} gift!`
+    }
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
